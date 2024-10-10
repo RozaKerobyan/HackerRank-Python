e7026990927516ae0e67410b3769b98e28999699
Roza Kerobyan <97788153+RozaKerobyan@users.noreply.github.com>
2024-10-10 20:19:37 +0400

    Update swap_case.py

--- swap_case.py
+++ swap_case.py
@@ -4,8 +4,13 @@ def swap_case(s):
     for i in s_list:
         if i.isalpha() and i.isupper():
             change_s.append(i.lower())
-        elif i.isalpha and i.islower():
+        elif i.isalpha() and i.islower():
             change_s.append(i.upper())
         else:
             change_s.append(i)
     return "".join(change_s)
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)   
